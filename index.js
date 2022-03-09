4f5c2797878d40bb9fb085fe028ee3b790c9a2ea
Vladislav Migachev <rolledm@gmail.com>
2022-03-09 03:23:58 +0300

    full res ++ capital letters

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fs from 'fs';
 
+let letter = '';
 let output = '';
 let file = fs.readFileSync('./res/res.csv', 'utf8').split('\n');
 let arr = []
@@ -8,11 +9,17 @@ let arr = []
 for (let iter in file) {
     let temp = file[iter].split(';');
     arr.push([temp[0], temp[1]])
-    output += '<p><a href=#' + iter + '>' + temp[0] + '</a><p>';
+    if (letter != temp[0][0]) {
+        letter = temp[0][0];
+        output += `<h2>${letter}</h2>`;
+    }
+    output += `<p><a href=#${iter}>${temp[0]}</a></p>`;
 }
 
+output += '<hr>'
+
 for (let iter in file) {
-    output += '<p><a name='+ iter + '><b>' + arr[iter][0] + '</b><br>' + arr[iter][1] + '</a></p>';
+    output += `<p><a name=${iter}><b>${arr[iter][0]}</b><br>${arr[iter][1]}</a></p>`;
 }
 
 let app = express();
