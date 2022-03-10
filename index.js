170fe2ccc1442d067dd733c2aa1d88f4d383bfd8
Vladislav Migachev <rolledm@gmail.com>
2022-03-11 00:37:35 +0300

    Loader moved to class

--- index.js
+++ index.js
@@ -1,25 +1,50 @@
 import express from 'express';
 import fs from 'fs';
 
+class CLoader {
+    SetLoaderStrategy(loaderStrategy) {
+        this.m_loaderStrategy = loaderStrategy;
+    }
+
+    LoadData() {
+        return this.m_loaderStrategy.LoadData();
+    }
+};
+
+class CLoadStrategyCSV {
+    LoadData() {
+        let file = fs.readFileSync('./res/res.csv', 'utf8').split('\n');
+        let arr = []
+
+        for (let iter in file) {
+            let temp = file[iter].split(';');
+            arr.push([temp[0], temp[1]])
+        }
+        return arr;
+    }
+};
+
 let letter = '';
 let output = '';
-let file = fs.readFileSync('./res/res.csv', 'utf8').split('\n');
-let arr = []
 
-for (let iter in file) {
-    let temp = file[iter].split(';');
-    arr.push([temp[0], temp[1]])
-    if (letter != temp[0][0]) {
-        letter = temp[0][0];
+let loader = new CLoader();
+let loaderStrategy = new CLoadStrategyCSV();
+loader.SetLoaderStrategy(loaderStrategy);
+
+let data = loader.LoadData();
+
+for (let iter in data) {
+    if (letter != data[iter][0][0]) {
+        letter = data[iter][0][0];
         output += `<h2>${letter}</h2>`;
     }
-    output += `<p><a href=#${iter}>${temp[0]}</a></p>`;
+    output += `<p><a href=#${iter}>${data[iter][0]}</a></p>`;
 }
 
 output += '<hr>'
 
-for (let iter in file) {
-    output += `<p><a name=${iter}><b>${arr[iter][0]}</b><br>${arr[iter][1]}</a></p>`;
+for (let iter in data) {
+    output += `<p><a name=${iter}><b>${data[iter][0]}</b><br>${data[iter][1]}</a></p>`;
 }
 
 let app = express();
